a50edc5907b0cd298c527ff62fad48f731352900
Sayan Ghosh <sgic2001@gmail.com>
2023-10-08 01:30:28 +0530

    fix breaking deployement of PERM_SESSION

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SESSION_TYPE'] = os.getenv('SESSION_TYPE')
-app.config['PERMANENT_SESSION_LIFETIME'] = int(os.getenv('PERMANENT_SESSION_LIFETIME'))
+app.config['PERMANENT_SESSION_LIFETIME'] = int(os.getenv('PERMANENT_SESSION_LIFETIME') or 365*86400)
 app.config['SESSION_PERMANENT'] = os.getenv('SESSION_PERMANENT')
 
 @app.before_request
